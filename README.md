c8676e9f6a1d0d6a42668493f65ad05d9c59b4b5
Noel O'Boyle <noel@nextmovesoftware.com>
2019-05-24 16:55:02 +0100

    Initial check in

---
+++ README.md
@@ -0,0 +1,3 @@
+# MolHash
+
+Command-line executable for the generation of "molecular hashes" for use in deduplication, sorting, or identifying related molecules.
---
+++ cmake/ReportRDKitVersion.cpp
@@ -0,0 +1,32 @@
+/*==============================================*/
+/* Copyright (c)  2018-2019  NextMove Software  */
+/*==============================================*/
+#include <stdio.h>
+
+#include <RDGeneral/versions.h>
+
+int main()
+{
+  unsigned int parts[3] = {0, 0, 0};
+
+  const char* version = RDKit::rdkitVersion;
+  const char *p = version;
+  unsigned int part = 0;
+  while(*p) {
+    if (*p >= '0' && *p <= '9') {
+      parts[part] *= 10;
+      parts[part] += *p - '0';
+    } else if ( *p == '.') {
+      part++;
+      if (part == 3)
+        break;
+    } else {
+      break; // e.g. the 'd' in "2019.03.01dev"
+    }
+    p++;
+  }
+
+  printf("%d", parts[0]*10000 + parts[1]*100 + parts[2]);
+  return 0;
+}
+
