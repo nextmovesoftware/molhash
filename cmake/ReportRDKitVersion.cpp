4ddf1a0954b64160c2397cdba28495bbae1ea849
Noel O'Boyle <noel@nextmovesoftware.com>
2019-05-28 10:20:39 +0100

    Update ReportRDKitVersion.cpp

--- cmake/ReportRDKitVersion.cpp
+++ cmake/ReportRDKitVersion.cpp
@@ -1,5 +1,11 @@
 /*==============================================*/
 /* Copyright (c)  2018-2019  NextMove Software  */
+/*                                              */
+/* This file is part of molhash.                */
+/*                                              */
+/* The contents are covered by the terms of the */
+/* BSD license, which is included in the file   */
+/* license.txt.                                 */
 /*==============================================*/
 #include <stdio.h>
 
